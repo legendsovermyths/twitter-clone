126253c6e053f7d9d7cc489c6e69660b164125f6
Mask Wolf <maskwolf2000@gmail.com>
2020-08-18 21:37:42 +0530

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
+import TwitterIcon from "@material-ui/icons/Twitter";
+import SidebarOption from "./SidebarOption.js";
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='app'>
+      <h1>Let's make a twitter clone!</h1>
+      <TwitterIcon />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+    </div>
+  );
 }
 
 export default App;
