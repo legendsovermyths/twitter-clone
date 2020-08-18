253841be165de8ecd11a91cc74b669f9bfe5a720
Mask Wolf <maskwolf2000@gmail.com>
2020-08-18 22:44:29 +0530

    add transition on sidebar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 
-import TwitterIcon from "@material-ui/icons/Twitter";
-import SidebarOption from "./SidebarOption.js";
 function App() {
   return (
     <div className='app'>
       <h1>Let's make a twitter clone!</h1>
-      <TwitterIcon />
-      <SidebarOption />
-      <SidebarOption />
-      <SidebarOption />
-      <SidebarOption />
-      <SidebarOption />
-      <SidebarOption />
+      <Sidebar />
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import ListIcon from "@material-ui/icons/List";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Sidebar() {
   return (
     <div className='sidebar'>
       <TwitterIcon />
+      <SidebarOption text='Home' Icon={HomeIcon} />
+      <SidebarOption text='Explore' Icon={SearchIcon} />
+      <SidebarOption text='Notification' Icon={NotificationsNoneIcon} />
+      <SidebarOption text='Messages' Icon={MailOutlineIcon} />
+      <SidebarOption text='Bookmarks' Icon={BookmarkBorderIcon} />
+      <SidebarOption text='List' Icon={ListIcon} />
+      <SidebarOption text='Profile' Icon={PermIdentityIcon} />
+      <SidebarOption text='More' Icon={MoreHorizIcon} />
     </div>
   );
 }
